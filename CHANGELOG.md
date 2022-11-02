b9aadd7ec0277a6f15110bf77ce23b5f1273fd79
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-11-02 11:10:48 +0100

    AZ-883: Lint code, fix deprecated HCL syntax

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# Unreleased
+
+Fixed
+  * AZ-883: Lint code, fix deprecated HCL syntax
+
 # v6.0.0 - 2022-07-01
 
 Breaking
--- locals.tf
+++ locals.tf
@@ -9,7 +9,7 @@ locals {
 
   default_redis_config = {
     rdb_backup_enabled            = local.data_persistence_enabled
-    rdb_storage_connection_string = local.data_persistence_enabled ? join("", azurerm_storage_account.redis_storage.*.primary_blob_connection_string) : null
+    rdb_storage_connection_string = one(azurerm_storage_account.redis_storage[*].primary_blob_connection_string)
     rdb_backup_frequency          = local.data_persistence_enabled ? var.data_persistence_frequency_in_minutes : null
     rdb_backup_max_snapshot_count = local.data_persistence_enabled ? var.data_persistence_max_snapshot_count : null
   }
