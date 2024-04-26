fa17b7b7e5cbe5838146acf59e3c4628b35d439c
Jérôme Respaut <jerome.respaut@claranet.com>
2024-04-26 15:51:39 +0200

    style(output): remove unused version from outputs-module

--- outputs-module.tf
+++ outputs-module.tf
@@ -2,7 +2,6 @@ output "terraform_module" {
   description = "Information about this Terraform module"
   value = {
     name       = "redis"
-    version    = file("${path.module}/VERSION")
     provider   = "azurerm"
     maintainer = "claranet"
   }
